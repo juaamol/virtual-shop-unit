1bf2777a6a5906422d12a795a17ae65f5f779e73
juaamol <jambulud@gmail.com>
2023-06-30 18:00:24 +0200

    Import http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
