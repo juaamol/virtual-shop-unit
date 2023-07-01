fb4103e0b6ec610d3041c277ab167b80ddddff2a
juaamol <jambulud@gmail.com>
2023-07-01 22:57:14 +0200

    add image gallery

---
+++ src/app/product/image-gallery/image-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.scss'],
+})
+export class ImageGalleryComponent {
+  @Input() images: string[] = [];
+  @Input() alt: string = '';
+}
