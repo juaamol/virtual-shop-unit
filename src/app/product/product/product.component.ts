42a075c5579ce5b7e06a0d3caccb1008b1d5f606
juaamol <jambulud@gmail.com>
2023-07-01 21:23:14 +0200

    show id of product when its card gets clicked

--- src/app/product/product/product.component.ts
+++ src/app/product/product/product.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
+  id$!: Observable<string | null>;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id$ = this.idFromUrl();
+  }
+
+  idFromUrl() {
+    return this.route.paramMap.pipe(map((paramMap) => paramMap.get('id')));
+  }
 }
