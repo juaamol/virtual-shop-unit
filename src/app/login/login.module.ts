bc2265d04ad9581e1fd164cfb414016b4cb328f6
juaamol <jambulud@gmail.com>
2023-07-02 13:18:32 +0200

    import router module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
 })
 export class LoginModule {}
--- src/app/login/login/login.component.spec.ts
+++ src/app/login/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { AuthService } from '../services/auth/auth.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,7 +12,7 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
       providers: [{ provide: AuthService, useValue: {} }],
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
