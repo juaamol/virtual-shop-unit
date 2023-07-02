87f692b3e0962478aca7196c166621e7cdbac458
juaamol <jambulud@gmail.com>
2023-07-02 12:53:38 +0200

    add auth service

---
+++ src/app/login/services/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { User } from 'src/app/data/types/User';
+import { filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly userKey = 'currentUser';
+
+  constructor(private userService: UserService) {}
+
+  login(email: string, password: string) {
+    return this.userService
+      .getUserByEmail(email)
+      .pipe(filter((user) => user !== undefined))
+      .subscribe({
+        next: (user) => {
+          if (user!.password === password) {
+            localStorage.setItem(this.userKey, JSON.stringify(user));
+          }
+
+          return user!.password === password;
+        },
+        error: () => false,
+      });
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.userKey);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem(this.userKey);
+  }
+
+  getCurrentUser(): User | null {
+    const userString = localStorage.getItem(this.userKey);
+    return userString ? JSON.parse(userString) : null;
+  }
+}
