b31108e05dc91874b350fb860924bac2b15b4d13
juaamol <jambulud@gmail.com>
2023-07-02 12:53:48 +0200

    add url for users

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   productsAPIUrl: 'https://api.escuelajs.co/api/v1/products/',
   categoriesAPIUrl: 'https://api.escuelajs.co/api/v1/categories',
+  usersAPIUrl: 'https://api.escuelajs.co/api/v1/users',
   typingDelayMs: 300,
   minQueryLength: 3,
 };
