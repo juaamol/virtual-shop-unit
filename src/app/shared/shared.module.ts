f5cb4a2de00c7166729e3ac4bb780d0de44ce7e4
juaamol <jambulud@gmail.com>
2023-07-03 15:07:08 +0200

    create shared gallery

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LazyLoaderDirective } from './lazy-loader/lazy-loader.directive';
 import { IntersectingDirective } from './intersecting/intersecting.directive';
 import { LoggedInGuard } from './guards/logged-in/logged-in.guard';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
-  declarations: [LazyLoaderDirective, IntersectingDirective],
+  declarations: [LazyLoaderDirective, IntersectingDirective, GalleryComponent],
   imports: [CommonModule],
-  exports: [LazyLoaderDirective, IntersectingDirective],
+  exports: [LazyLoaderDirective, IntersectingDirective, GalleryComponent],
   providers: [LoggedInGuard],
 })
 export class SharedModule {}
