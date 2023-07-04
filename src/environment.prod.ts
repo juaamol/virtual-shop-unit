1efd2bd9a2a222d7d7b7499d6dfa7268a1286873
juaamol <jambulud@gmail.com>
2023-07-04 15:26:27 +0200

    add links to cache on ngsw

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   productsAPIUrl: 'https://api.escuelajs.co/api/v1/products/',
   categoriesAPIUrl: 'https://api.escuelajs.co/api/v1/categories',
   usersAPIUrl: 'https://api.escuelajs.co/api/v1/users',
