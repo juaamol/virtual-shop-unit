b5850d44e085083d0b70e930794fca9b9d441d8a
juaamol <jambulud@gmail.com>
2023-07-05 11:44:34 +0200

    remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { ResultsModule } from './results/results.module';
-import { ProductModule } from './product/product.module';
-import { LoginModule } from './login/login.module';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
-import { CategoriesModule } from './categories/categories.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
@@ -20,9 +17,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     HttpClientModule,
     HomeModule,
     ResultsModule,
-    ProductModule,
-    LoginModule,
-    CategoriesModule,
     SharedModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
